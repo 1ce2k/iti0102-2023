ef325fe748f2d0fd58133e5ec001cdba386b047b
Danil Zimarev <dazima@taltech.ee>
2023-09-06 18:02:33 +0300

    ex2 hello and poem test

---
+++ EX/ex02_beggining/poem.py
@@ -0,0 +1,20 @@
+"""EX02 Poem."""
+
+"""
+Ask for user's inputs and print out a poem in one line using f string and newline (\n)
+
+Example input:
+color = "red"
+objects = "violets"
+activity = "code"
+
+Example output:
+Roses are red,
+violets are blue,
+I love to code
+And so will you!
+"""
+color = input("Enter a color: ")
+objects = input("Enter some objects: ")
+activity = input("Enter some activity: ")
+print(f"Roses are {color}\n{objects} are bluen\nI love {activity}\nAnd so will you!")
