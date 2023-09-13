e90fc38a981d7bb9c66967aba92f5c8470ad7e4b
Danil Zimarev <dazima@taltech.ee>
2023-09-13 21:01:12 +0300

    bees simple test3

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -1,86 +1,90 @@
 """Whether bees meet."""
 
 
-def do_bees_meet(honeycomb_width: int, honeyhopper_data: str, pollenpadle_data: str) -> bool:
+def do_bees_meet(honeycomb_width: int, honey_hopper_data: str, pollen_paddle_data: str) -> bool:
     """Return whether bees meet."""
-    if not honeycomb_width > 0 or len(honeyhopper_data.split(',')) < 4 or len(pollenpadle_data.split(',')) < 4:
+    # Validate the input
+    if not honeycomb_width > 0 or len(honey_hopper_data.split(',')) < 4 or len(pollen_paddle_data.split(',')) < 4:
         raise ValueError("Insufficient data for sequence identification")
-    #
-    # def movement_pattern(sequence):
-    #     end = len(sequence)
-    #     for i in range(2, end):
-    #         # arithmetic sequence
-    #         if sequence[end - 1] - sequence[end - 2] == sequence[i] - sequence[i - 1] == sequence[1] - sequence[0] != 0:
-    #             print(0)
-    #             return 0
-    #         # if one is not moving
-    #         elif sequence[i] - sequence[i - 1] == sequence[1] - sequence[0] == sequence[end - 1] - sequence[end - 2] == 0:
-    #             print(1)
-    #             return 1
-    #         # geometric sequence
-    #         elif sequence[end - 1] / sequence[end - 2] == sequence[i] / sequence[i - 1] == sequence[1] / sequence[0]:
-    #             print(2)
-    #             return 2
-    #         # sequence where different is in geometrical sequence 1,3,7,15
-    #         elif int(sequence[end - 1] - sequence[end - 2]) == int(sequence[i] - sequence[i - 1]) * i == \
-    #                 int(sequence[1] - sequence[0]) * i * i:
-    #             print(3)
-    #             return 3
-    #             # sequence where different is in arithmetic sequence 1,2,4,7 or 5,9,17,29
-    #         elif sequence[1] - sequence[0] == (sequence[i] - sequence[i - 1]) / i == \
-    #                 (sequence[end - 1] - sequence[end - 2]) / (end - 1):
-    #             print(4)
-    #             return 4
-    #
-    #         else:
-    #             raise ValueError("Insufficient data for sequence identification")
-    #
-    # honey_moves = list(map(int, honeyhopper_data.split(',')))
-    # pollen_moves = list(map(int, pollenpadle_data.split(',')))
-    # honey_pattern = movement_pattern(honey_moves)
-    # pollen_pattern = movement_pattern(pollen_moves)
-    #
-    # comb_length = int(honeycomb_width / 2) + 1
-    # count = comb_length
-    # while comb_length < honeycomb_width:
-    #     count = count + comb_length + 1
-    #     comb_length += 1
-    # count = count * 2 - honeycomb_width
-    # honey_pos = 0
-    # pollen_pos = count
-    #
-    # if honey_pattern == 0:
-    #     if pollen_pattern == 0:
-    #         if honey_pos == pollen_pos:
-    #             return True
-    # if honey_pattern == 4 or pollen_pattern == 4:
-    #     return True
 
-    # Parse movement data strings into lists of integers
-    honeyhopper_moves = [int(x) for x in honeyhopper_data.split(',')]
-    pollenpaddle_moves = [int(x) for x in pollenpadle_data.split(',')]
+    # Parse input
+    honey_hopper_positions = list(map(int, honey_hopper_data.split(',')))
+    pollen_paddle_positions = list(map(int, pollen_paddle_data.split(',')))
 
-    # Initialize starting positions of both bees
-    honeyhopper_position = (0, 0)
-    pollenpaddle_position = (0, 0)
+    # Analyze movement pattern
+    honey_hopper_pattern = analyze_movement_pattern(honey_hopper_positions)
+    pollen_paddle_pattern = analyze_movement_pattern(pollen_paddle_positions)
 
-    # Iterate through movement data for both bees
-    for h_move, p_move in zip(honeyhopper_moves, pollenpaddle_moves):
-        # Update positions based on movement data
-        honeyhopper_position = (honeyhopper_position[0] + h_move, honeyhopper_position[1] + h_move)
-        pollenpaddle_position = (pollenpaddle_position[0] + p_move, pollenpaddle_position[1] + p_move)
+    # Calculate position
+    honeyhopper_hex_positions = calculate_position(honey_hopper_positions, honey_hopper_pattern, honeycomb_width)
+    pollenpadle_hex_positions = calculate_position(pollen_paddle_positions, pollen_paddle_pattern, honeycomb_width)
 
-        # Check if they land on the same hex
-        if honeyhopper_position == pollenpaddle_position:
+    # Check gor intersection
+    for honeyhopper_pos in honeyhopper_hex_positions:
+        if honeyhopper_pos in pollenpadle_hex_positions:
             return True
-
-    # If they never land on the same hex, return False
-    return False
-
     return False
 
 
-print(do_bees_meet(3, '1,2,3,4', '1,2,3,4'))  # =>7, 0, 1, False
-print(do_bees_meet(3, '1,2,3,4', '5,9,17,33'))  # =>7, 0, 2, False
-print(do_bees_meet(3, '1,2,3,4', '1,2,4,7'))  # =>7, 0, 3, False
-print(do_bees_meet(3, '0,0,0,0', '1,2,4,7'))  # =>7, 0, 3, False
+def analyze_movement_pattern(positions):
+    """Return movement pattern"""
+    constant_gap = True
+    gap = positions[1] - positions[0]
+    for i in range(2, len(positions)):
+        if positions[i] - positions[i - 1] != gap:
+            constant_gap = False
+            break
+    if constant_gap:
+        return 'constant_gap'
+
+    increasing_gap = True
+    for i in range(1, len(positions)):
+        if positions[i + 1] - positions[i] <= positions[i] - positions[i - 1]:
+            increasing_gap = False
+            break
+    if increasing_gap:
+        return "increasing_gap"
+
+    geometric_sequence = True
+    ratio = positions[1] // positions[0]
+    for i in range(1, len(positions) - 1):
+        if positions[i + 1] // positions[i] != ratio:
+            geometric_sequence = False
+            break
+    if geometric_sequence:
+        return "geometric_sequence"
+
+    increasing_geometric_gap = True
+    for i in range(1, len(positions)):
+        if positions[i + 1] // positions[i] <= positions[i] // positions[i - 1]:
+            increasing_geometric_gap = False
+            break
+    if increasing_geometric_gap:
+        return "increasing_geometric_gap"
+
+    raise ValueError("Pattern not recognized")
+
+
+def calculate_position(positions, pattern, honeycomb_width):
+    calculated_positions = []
+
+    if pattern == "constant_gap":
+        gap = positions[1] - positions[0]
+        for i in range(len(positions)):
+            calculated_positions.append(positions[i] + gap * i)
+    elif pattern == "increasing_gap":
+        for i in range(len(positions)):
+            calculated_positions.append(positions[i] + (i * (i + 1) // 2))
+    elif pattern == "geometric_sequence":
+        ratio = positions[1] // positions[0]
+        for i in range(len(positions)):
+            calculated_positions.append(positions[i] * (ratio ** i))
+    elif pattern == "increasing_geometric_gap":
+        for i in range(len(positions)):
+            calculated_positions.append(positions[i] * (2 ** i))
+    return calculated_positions
+
+print(do_bees_meet(3, '1,2,3,4', '1,2,3,4'))  # =>7, 0, 1, True
+# print(do_bees_meet(3, '1,2,3,4', '5,9,17,33'))  # =>7, 0, 2, False
+# print(do_bees_meet(3, '1,2,3,4', '1,2,4,7'))  # =>7, 0, 3, False
+# print(do_bees_meet(3, '0,0,0,0', '1,2,4,7'))  # =>7, 0, 3, False
