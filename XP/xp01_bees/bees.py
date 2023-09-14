9f302e9b711e07ed7d887b32bae067078c205de5
Danil Zimarev <dazima@taltech.ee>
2023-09-14 15:21:12 +0300

    fix 1

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -55,115 +55,143 @@ def analyze_movement_pattern(sequence):
             raise ValueError("Insufficient data for sequence identification")
 
 
-def calculate_position(positions, start_pos, pattern, comb_size, who_is_moving):
-    """Return list of bee positions."""
-    calculated_positions = []
+# def calculate_position(positions, start_pos, pattern, comb_size, who_is_moving):
+#     """Return list of bee positions."""
+#     calculated_positions = []
+#     d = positions[1] - positions[0]
+#     # d2 = positions[2] - positions[1]
+#     q = positions[1] / positions[0]
+#     q2 = positions[2] / positions[1]
+#     if who_is_moving == 'h':
+#         if pattern == 0:
+#             for n in range(1, comb_size + 1):
+#                 position = start_pos + d * (n - 1)
+#                 if 1 <= position <= comb_size:
+#                     calculated_positions.append(position)
+#                 else:
+#                     position = position % comb_size
+#                     if 1 <= position <= comb_size:
+#                         calculated_positions.append(position)
+#                     elif position == 0:
+#                         position = position + d * (n - 1)
+#                         calculated_positions.append(position)
+#         elif pattern == 1:
+#             for i in range(1, comb_size + 1):
+#                 position = start_pos
+#                 calculated_positions.append(position)
+#         elif pattern == 2:
+#             calculated_positions.append(start_pos)
+#             for n in range(1, comb_size):
+#                 position = int(positions[1] * q ** (n - 1))
+#                 if 1 <= position <= comb_size:
+#                     calculated_positions.append(position)
+#                 else:
+#                     position = int(position % comb_size)
+#                     if 1 <= position <= comb_size:
+#                         calculated_positions.append(position)
+#                     elif position == 0:
+#                         position = start_pos
+#                         calculated_positions.append(position)
+#         elif pattern == 3:
+#             calculated_positions.append(start_pos)
+#             for n in range(1, comb_size):
+#                 step = d * q2 ** (n - 1)
+#                 position = int(calculated_positions[n - 1] + step)
+#                 if 1 <= position <= comb_size and step <= comb_size:
+#                     calculated_positions.append(position)
+#                 else:
+#                     position = position % comb_size
+#                     if 1 <= position <= comb_size:
+#                         calculated_positions.append(position)
+#                     elif position == 0:
+#                         position = start_pos
+#                         calculated_positions.append(position)
+#     if who_is_moving == 'p':
+#         if pattern == 0:
+#             for n in range(1, comb_size + 1):
+#                 position = start_pos - d * (n - 1)
+#                 if 1 <= position <= comb_size:
+#                     calculated_positions.append(position)
+#                 else:
+#                     position = position % comb_size
+#                     if 1 <= position <= comb_size:
+#                         calculated_positions.append(position)
+#                     elif position == 0:
+#                         position = abs(d * (n - 1))
+#                         calculated_positions.append(position)
+#         elif pattern == 1:
+#             position = start_pos
+#             for n in range(1, comb_size + 1):
+#                 position = start_pos
+#                 calculated_positions.append(position)
+#         elif pattern == 2:
+#             calculated_positions.append(start_pos)
+#             for n in range(1, comb_size + 1):
+#                 position = int(calculated_positions[n - 1] - (positions[0] * q ** (n - 1)))
+#                 if 1 <= position <= comb_size:
+#                     calculated_positions.append(position)
+#                 else:
+#                     position = int(position % comb_size)
+#                     if 1 <= position <= comb_size:
+#                         calculated_positions.append(position)
+#                     elif position == 0:
+#                         position = start_pos
+#                         calculated_positions.append(position)
+#         elif pattern == 3:
+#             calculated_positions.append(start_pos)
+#             for n in range(1, comb_size):
+#                 step = d * q2 ** (n - 1)
+#                 position = int(calculated_positions[n - 1] - step)
+#                 if 1 <= position <= comb_size and step <= comb_size:
+#                     calculated_positions.append(position)
+#                 else:
+#                     position = position % comb_size
+#                     if 1 <= position <= comb_size:
+#                         calculated_positions.append(position)
+#                     elif position == 0:
+#                         position = start_pos
+#                         calculated_positions.append(position)
+#         elif pattern == 4:
+#             calculated_positions.append(start_pos)
+#             for n in range(1, comb_size):
+#                 position = calculated_positions[n - 1] - d * n
+#                 if 1 <= position <= comb_size:
+#                     calculated_positions.append(position)
+#                 else:
+#                     position = position % comb_size
+#                     if 1 <= position <= comb_size:
+#                         calculated_positions.append(position)
+#                     elif position == 0:
+#                         position = start_pos
+#                         calculated_positions.append(position)
+#
+#     return calculated_positions
+
+
+def calculate_position(positions, start_pos, pattern, comb_size, who):
+    calculated_positions = [start_pos]
+
     d = positions[1] - positions[0]
-    # d2 = positions[2] - positions[1]
     q = positions[1] / positions[0]
-    q2 = positions[2] / positions[1]
-    if who_is_moving == 'h':
-        if pattern == 0:
-            for n in range(1, comb_size + 1):
-                position = start_pos + d * (n - 1)
-                if 1 <= position <= comb_size:
-                    calculated_positions.append(position)
-                else:
-                    position = position % comb_size
-                    if 1 <= position <= comb_size:
-                        calculated_positions.append(position)
-                    elif position == 0:
-                        position = position + d * (n - 1)
-                        calculated_positions.append(position)
-        elif pattern == 1:
-            for i in range(1, comb_size + 1):
-                position = start_pos
-                calculated_positions.append(position)
-        elif pattern == 2:
-            calculated_positions.append(start_pos)
-            for n in range(1, comb_size):
-                position = int(positions[1] * q ** (n - 1))
-                if 1 <= position <= comb_size:
-                    calculated_positions.append(position)
-                else:
-                    position = int(position % comb_size)
-                    if 1 <= position <= comb_size:
-                        calculated_positions.append(position)
-                    elif position == 0:
-                        position = start_pos
-                        calculated_positions.append(position)
-        elif pattern == 3:
-            calculated_positions.append(start_pos)
-            for n in range(1, comb_size):
-                step = d * q2 ** (n - 1)
-                position = int(calculated_positions[n - 1] + step)
-                if 1 <= position <= comb_size and step <= comb_size:
-                    calculated_positions.append(position)
-                else:
-                    position = position % comb_size
-                    if 1 <= position <= comb_size:
-                        calculated_positions.append(position)
-                    elif position == 0:
-                        position = start_pos
-                        calculated_positions.append(position)
-    if who_is_moving == 'p':
-        if pattern == 0:
-            for n in range(1, comb_size + 1):
-                position = start_pos - d * (n - 1)
-                if 1 <= position <= comb_size:
-                    calculated_positions.append(position)
-                else:
-                    position = position % comb_size
-                    if 1 <= position <= comb_size:
-                        calculated_positions.append(position)
-                    elif position == 0:
-                        position = abs(d * (n - 1))
-                        calculated_positions.append(position)
-        elif pattern == 1:
-            position = start_pos
-            for n in range(1, comb_size + 1):
-                position = start_pos
-                calculated_positions.append(position)
-        elif pattern == 2:
-            calculated_positions.append(start_pos)
-            for n in range(1, comb_size + 1):
-                position = int(calculated_positions[n - 1] - (positions[0] * q ** (n - 1)))
-                if 1 <= position <= comb_size:
-                    calculated_positions.append(position)
-                else:
-                    position = int(position % comb_size)
-                    if 1 <= position <= comb_size:
-                        calculated_positions.append(position)
-                    elif position == 0:
-                        position = start_pos
-                        calculated_positions.append(position)
-        elif pattern == 3:
-            calculated_positions.append(start_pos)
-            for n in range(1, comb_size):
-                step = d * q2 ** (n - 1)
-                position = int(calculated_positions[n - 1] - step)
-                if 1 <= position <= comb_size and step <= comb_size:
-                    calculated_positions.append(position)
-                else:
-                    position = position % comb_size
-                    if 1 <= position <= comb_size:
-                        calculated_positions.append(position)
-                    elif position == 0:
-                        position = start_pos
-                        calculated_positions.append(position)
-        elif pattern == 4:
-            calculated_positions.append(start_pos)
-            for n in range(1, comb_size):
-                position = calculated_positions[n - 1] - d * n
-                if 1 <= position <= comb_size:
-                    calculated_positions.append(position)
-                else:
-                    position = position % comb_size
-                    if 1 <= position <= comb_size:
-                        calculated_positions.append(position)
-                    elif position == 0:
-                        position = start_pos
-                        calculated_positions.append(position)
+
+    if pattern == 0:
+        for n in range(1, comb_size + 1):
+            position = start_pos + (-1 if who == 'p' else 1) * d * (n - 1)
+            calculated_positions.append(position % comb_size or comb_size)
+    elif pattern == 2:
+        for n in range(1, comb_size):
+            position = int(calculated_positions[n - 1] + (-1 if who == 'p' else 1) * positions[0] * q ** (n - 1))
+            calculated_positions.append(position % comb_size or comb_size)
+    elif pattern == 3:
+        q2 = positions[2] // positions[1]
+        for n in range(1, comb_size):
+            step = d * q2 ** (n - 1)
+            position = int(calculated_positions[n - 1] + (-1 if who == 'p' else 1) * step)
+            calculated_positions.append(position % comb_size or comb_size)
+    elif pattern == 4:
+        for n in range(1, comb_size):
+            position = calculated_positions[n - 1] + (-1 if who == 'p' else 1) * d * n
+            calculated_positions.append(position % comb_size or comb_size)
 
     return calculated_positions
 
