9796b05c5e993b3105c3b2e65531498f7aecebbd
Danil Zimarev <dazima@taltech.ee>
2023-09-14 21:38:56 +0300

    bees fix

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -1,4 +1,4 @@
-"""Whether bees meet."""
+"""Test bees."""
 
 
 def do_bees_meet(honeycomb_width: int, honey_hopper_data: str, pollen_paddle_data: str) -> bool:
@@ -17,7 +17,11 @@ def do_bees_meet(honeycomb_width: int, honey_hopper_data: str, pollen_paddle_dat
 
     honey_comb_size = calculate_honeycomb_size(honeycomb_width)
     h_start_pos = honey_hopper_positions[0]
-    p_start_pos = honey_comb_size - pollen_paddle_positions[0]
+    p_start_pos = 0
+    if pollen_paddle_positions[0] == 1:
+        p_start_pos = honey_comb_size
+    else:
+        p_start_pos = honey_comb_size - pollen_paddle_positions[0]
     # Calculate position
     honey_hopper_hex_positions = calculate_position(honey_hopper_positions, h_start_pos, honey_hopper_pattern, honey_comb_size, 'h')
     pollen_paddle_hex_positions = calculate_position(pollen_paddle_positions, p_start_pos, pollen_paddle_pattern, honey_comb_size, 'p')
@@ -28,6 +32,17 @@ def do_bees_meet(honeycomb_width: int, honey_hopper_data: str, pollen_paddle_dat
             if honey_hopper_hex_positions[x] == pollen_paddle_hex_positions[i]:
                 return True
     return False
+
+
+def calculate_honeycomb_size(honeycomb_width) -> int:
+    """Return size of honey comb."""
+    if honeycomb_width <= 0:
+        return 0
+    cells_count = 0
+    for i in range(honeycomb_width):
+        cells_count = cells_count + honeycomb_width + i
+    cells_count = cells_count * 2 - (2 * honeycomb_width - 1)
+    return cells_count
 
 
 def analyze_movement_pattern(sequence):
@@ -53,163 +68,54 @@ def analyze_movement_pattern(sequence):
             raise ValueError("Insufficient data for sequence identification")
 
 
-# def calculate_position(positions, start_pos, pattern, comb_size, who_is_moving):
-#     """Return list of bee positions."""
-#     calculated_positions = []
-#     d = positions[1] - positions[0]
-#     # d2 = positions[2] - positions[1]
-#     q = positions[1] / positions[0]
-#     q2 = positions[2] / positions[1]
-#     if who_is_moving == 'h':
-#         if pattern == 0:
-#             for n in range(1, comb_size + 1):
-#                 position = start_pos + d * (n - 1)
-#                 if 1 <= position <= comb_size:
-#                     calculated_positions.append(position)
-#                 else:
-#                     position = position % comb_size
-#                     if 1 <= position <= comb_size:
-#                         calculated_positions.append(position)
-#                     elif position == 0:
-#                         position = position + d * (n - 1)
-#                         calculated_positions.append(position)
-#         elif pattern == 1:
-#             for i in range(1, comb_size + 1):
-#                 position = start_pos
-#                 calculated_positions.append(position)
-#         elif pattern == 2:
-#             calculated_positions.append(start_pos)
-#             for n in range(1, comb_size):
-#                 position = int(positions[1] * q ** (n - 1))
-#                 if 1 <= position <= comb_size:
-#                     calculated_positions.append(position)
-#                 else:
-#                     position = int(position % comb_size)
-#                     if 1 <= position <= comb_size:
-#                         calculated_positions.append(position)
-#                     elif position == 0:
-#                         position = start_pos
-#                         calculated_positions.append(position)
-#         elif pattern == 3:
-#             calculated_positions.append(start_pos)
-#             for n in range(1, comb_size):
-#                 step = d * q2 ** (n - 1)
-#                 position = int(calculated_positions[n - 1] + step)
-#                 if 1 <= position <= comb_size and step <= comb_size:
-#                     calculated_positions.append(position)
-#                 else:
-#                     position = position % comb_size
-#                     if 1 <= position <= comb_size:
-#                         calculated_positions.append(position)
-#                     elif position == 0:
-#                         position = start_pos
-#                         calculated_positions.append(position)
-#     if who_is_moving == 'p':
-#         if pattern == 0:
-#             for n in range(1, comb_size + 1):
-#                 position = start_pos - d * (n - 1)
-#                 if 1 <= position <= comb_size:
-#                     calculated_positions.append(position)
-#                 else:
-#                     position = position % comb_size
-#                     if 1 <= position <= comb_size:
-#                         calculated_positions.append(position)
-#                     elif position == 0:
-#                         position = abs(d * (n - 1))
-#                         calculated_positions.append(position)
-#         elif pattern == 1:
-#             position = start_pos
-#             for n in range(1, comb_size + 1):
-#                 position = start_pos
-#                 calculated_positions.append(position)
-#         elif pattern == 2:
-#             calculated_positions.append(start_pos)
-#             for n in range(1, comb_size + 1):
-#                 position = int(calculated_positions[n - 1] - (positions[0] * q ** (n - 1)))
-#                 if 1 <= position <= comb_size:
-#                     calculated_positions.append(position)
-#                 else:
-#                     position = int(position % comb_size)
-#                     if 1 <= position <= comb_size:
-#                         calculated_positions.append(position)
-#                     elif position == 0:
-#                         position = start_pos
-#                         calculated_positions.append(position)
-#         elif pattern == 3:
-#             calculated_positions.append(start_pos)
-#             for n in range(1, comb_size):
-#                 step = d * q2 ** (n - 1)
-#                 position = int(calculated_positions[n - 1] - step)
-#                 if 1 <= position <= comb_size and step <= comb_size:
-#                     calculated_positions.append(position)
-#                 else:
-#                     position = position % comb_size
-#                     if 1 <= position <= comb_size:
-#                         calculated_positions.append(position)
-#                     elif position == 0:
-#                         position = start_pos
-#                         calculated_positions.append(position)
-#         elif pattern == 4:
-#             calculated_positions.append(start_pos)
-#             for n in range(1, comb_size):
-#                 position = calculated_positions[n - 1] - d * n
-#                 if 1 <= position <= comb_size:
-#                     calculated_positions.append(position)
-#                 else:
-#                     position = position % comb_size
-#                     if 1 <= position <= comb_size:
-#                         calculated_positions.append(position)
-#                     elif position == 0:
-#                         position = start_pos
-#                         calculated_positions.append(position)
-#
-#     return calculated_positions
-
-
 def calculate_position(positions, start_pos, pattern, comb_size, who):
-    calculated_positions = [start_pos]
+    calculated_positions = []
 
+    # calculate difference and ratio for sequence
     d = positions[1] - positions[0]
     q = positions[1] / positions[0]
 
+    # calculate for arithmetic
     if pattern == 0:
         for n in range(1, comb_size + 1):
             position = start_pos + (-1 if who == 'p' else 1) * d * (n - 1)
             calculated_positions.append(position % comb_size or comb_size)
-    elif pattern == 2:
+
+    # calculate for not moving
+    if pattern == 1:
+        for n in range(1, comb_size + 1):
+            position = start_pos
+            calculated_positions.append(start_pos)
+
+    # calculate for geometric sequence
+    if pattern == 2:
         for n in range(1, comb_size):
             position = int(calculated_positions[n - 1] + (-1 if who == 'p' else 1) * positions[0] * q ** (n - 1))
             calculated_positions.append(position % comb_size or comb_size)
-    elif pattern == 3:
+
+    # sequence where different is in geometrical sequence
+    if pattern == 3:
         q2 = positions[2] // positions[1]
         for n in range(1, comb_size):
             step = d * q2 ** (n - 1)
             position = int(calculated_positions[n - 1] + (-1 if who == 'p' else 1) * step)
             calculated_positions.append(position % comb_size or comb_size)
-    elif pattern == 4:
+
+    # sequence where different is in arithmetic sequence 1,2,4,7 or 5,9,17,29
+    if pattern == 4:
+        calculated_positions.append(start_pos)
         for n in range(1, comb_size):
             position = calculated_positions[n - 1] + (-1 if who == 'p' else 1) * d * n
             calculated_positions.append(position % comb_size or comb_size)
-
+    print(calculated_positions)
+    print(comb_size)
+    print(pattern)
     return calculated_positions
 
 
-def calculate_honeycomb_size(honeycomb_width) -> int:
-    """Return size of honey comb."""
-    if honeycomb_width <= 0:
-        return 0
-    cells_count = 0
-    for i in range(honeycomb_width):
-        cells_count = cells_count + honeycomb_width + i
-    cells_count = cells_count * 2 - (2 * honeycomb_width - 1)
-    return cells_count
-
-
-print(do_bees_meet(3, '1,2,3,4', '1,2,3,4'))  # => 0, 0, True
-print(do_bees_meet(923, '1,4,7,10', '1,2,4,7'))  # => 0, 0, True
-
-print(do_bees_meet(3, '1,1,1,1', '1,2,3,4'))  # =>7, 1, 1, True
-print(do_bees_meet(3, '1,1,1,1', '1,2,3,4'))  # =>7, 1, 1, False
-print(do_bees_meet(3, '1,2,4,8', '2,6,18,54'))  # =>7, 2, 2, True
-print(do_bees_meet(3, '1,3,7,15', '5,9,17,33'))  # =>7, 3, 3, True
-print(do_bees_meet(3, '1,2,4,7', '5,9,17,29'))  # =>7, 4, 4, True
+if __name__ == "__main__":
+    print(do_bees_meet(15457, '1,2,4,7', '1,2,4,7'))
+    # print(calculate_honeycomb_size(5))
+    # print(calculate_honeycomb_size(23))
+    # print(calculate_honeycomb_size(30))
+    # print(calculate_honeycomb_size(50))
