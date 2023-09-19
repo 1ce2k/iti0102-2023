a74638e228adb754555685bed6bf82bc498077ae
Danil Zimarev <dazima@taltech.ee>
2023-09-19 13:49:27 +0300

    fix 1

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -35,8 +35,10 @@ def do_bees_meet(honeycomb_width: int, honey_hopper_data: str, pollen_paddle_dat
         p_pos = honey_comb_size
     elif pollen_paddle_positions[0] == -1:
         p_pos = abs(pollen_paddle_positions[0])
+    # elif honey_comb_size - pollen_paddle_positions[0] == 0:
+    #     p_pos = honey_comb_size
     else:
-        p_pos = honey_comb_size - pollen_paddle_positions[0] + 1
+        p_pos = (honey_comb_size - pollen_paddle_positions[0] + 1) % honey_comb_size
     print(h_pos, p_pos)
     # Check for intersection
     for i in range(1, honey_comb_size):
@@ -154,9 +156,10 @@ def calculate_step(positions, pattern, who) -> int:
 
 if __name__ == "__main__":
     # print(do_bees_meet(300, "1,2,3,4,5", "1,2,3,4,5"))      # True
-    print(do_bees_meet(3, '2,4,6,8', '1,2,3,4'))              #True
+    # print(do_bees_meet(3, '2,4,6,8', '1,2,3,4'))              #True
+    print(do_bees_meet(3, '2,4,6,8', '19,21,23,25'))              #True
     print(do_bees_meet(3, '2,4,6,8', '2,4,6,8'))              #True
-    assert do_bees_meet(5, "1,2,4,7", "2,4,8,14") is True
+    # assert do_bees_meet(5, "1,2,4,7", "2,4,8,14") is True
     # print(do_bees_meet(300, '1,2,4,8', '1,2,4,8'))          # True
     # print(do_bees_meet(3, '-1,-2,-3,-4', '-1,-2,-3,-4'))    # True
     # print(do_bees_meet(400, '1,3,7,15', '1,3,7,15'))    # True
