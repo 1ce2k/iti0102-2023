21f26adbb4637da9695ee019bf6869e4126d5f5a
Danil Zimarev <dazima@taltech.ee>
2023-09-21 19:54:13 +0300

    bees fix

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -16,19 +16,20 @@ def do_bees_meet(honeycomb_width: int, honeyhopper_data: str, pollenpadle_data: 
     if h_pattern == p_pattern == 'standing':
         if h_moves[0] != p_moves[0]:
             return False
-    count = 1
+    count = 0
     index1 = 1
     index2 = 1
     while count <= hex_size:
+        # if h_moves[index1] == p_moves[index2]:
+        #     return True
+        index1 = (index1 + 1) % len(h_moves)
+        index2 = (index2 + 1) % len(p_moves)
         if h_moves[index1] == p_moves[index2]:
             return True
-        else:
-            index1 = (index1 + 1) % len(h_moves)
-            index2 = (index2 + 1) % len(p_moves)
-            h_moves = honey_next_pos(h_moves[index1], h_pattern, hex_size, h_moves)
-            p_moves = pollen_next_pos(p_moves[index2], p_pattern, hex_size, p_moves, p_steps)
+        h_moves = honey_next_pos(h_moves[index1], h_pattern, hex_size, h_moves)
+        p_moves = pollen_next_pos(p_moves[index2], p_pattern, hex_size, p_moves, p_steps)
         count += 1
-        # print(h_moves, p_moves)
+        print(h_moves, p_moves)
     return False
 
 
@@ -158,7 +159,7 @@ def pollen_next_pos(position: int, p_pattern: str, hex_size: int, p_moves: list,
     """Find pollen bee next position."""
     pos = position
     if p_pattern == 'standing':
-        pos = position
+        return p_moves
     if p_pattern == 'arithmetic':
         pos = (pos - (p_steps[1] - p_steps[0]))
     if p_pattern == 'geometric':
@@ -253,12 +254,12 @@ if __name__ == '__main__':
     # print(pollen_next_pos(58, 'arithmetic', 61, [1, 2, 3, 4]))  # => [57]
     # print(pollen_next_pos(30, 'geometric', 61, [61, 60, 58, 54, 46, 30], [1, 2, 4, 8]))
 
-    # assert do_bees_meet(5, '1,2,3,4', '1,1,1,1') is True
-    # assert do_bees_meet(5, '1,1,1,1', '1,2,3,4') is True
-    # assert do_bees_meet(300, '1,2,3,4', '1,2,3,4') is True
-    # assert do_bees_meet(500, '1,2,4,8', '1,2,4,8') is True
-    assert do_bees_meet(5, '1,3,7,15', '1,3,7,15') is True
-    # assert do_bees_meet(5, "1,2,4,7", "2,4,8,14") is True
+    assert do_bees_meet(3, '1,2,3,4', '1,1,1,1') is True
+    # assert do_bees_meet(3, '1,1,1,1', '1,2,3,4') is True
+    # assert do_bees_meet(3, '1,2,3,4', '1,2,3,4') is True
+    # assert do_bees_meet(3, '1,2,4,8', '1,2,4,8') is True
+    # assert do_bees_meet(3, '1,3,7,15', '1,3,7,15') is True
+    # assert do_bees_meet(3, "1,2,4,7", "2,4,8,14") is True
     # sequence_1 = ",".join(str(x) for x in range(50000, 200001, 10000))  # Arithmetic sequence with a large difference
     # sequence_2 = ",".join(
     #     str(2 ** x) for x in range(30, 45))  # Geometric sequence with a ratio of 2, but starting from a larger power
