9346d947e3015ec1cb59a7e8b782c91c583555c2
Danil Zimarev <dazima@taltech.ee>
2023-09-22 22:28:53 +0300

    bees fix

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -28,7 +28,7 @@ def do_bees_meet(honeycomb_width: int, honeyhopper_data: str, pollenpadle_data: 
             p_temp = p_pos
             h_pos = honey_next_pos(h_pos, h_pattern, hex_size, h_steps, i)
             p_pos = pollen_next_pos(p_pos, p_pattern, hex_size, p_steps, i)
-        print(h_pos, p_pos)
+        # print(h_pos, p_pos)
         if h_pos == h_temp and p_pos == p_temp:
             return False
     return False
@@ -182,7 +182,7 @@ def pollen_start_pos(step: int, hex_size: int) -> int:
     if step == 1:
         return hex_size
     if step % hex_size == 0:
-        return hex_size - step
+        return 1
     return hex_size - step % hex_size + 1
 
 
@@ -254,15 +254,12 @@ if __name__ == '__main__':
     # assert do_bees_meet(5, '1,2,4,8', '1,2,4,8') is True
     # assert do_bees_meet(5, '1,3,7,15', '1,3,7,15') is True
     # assert do_bees_meet(5, "1,2,4,7", "2,4,8,14") is True
-    # sequence_1 = ",".join(str(x) for x in range(50000, 200001, 10000))  # Arithmetic sequence with a large difference
-    # assert do_bees_meet(2, sequence_1, sequence_1) is True
-    # sequence_2 = ",".join(
-    #     str(2 ** x) for x in range(30, 45))  # Geometric sequence with a ratio of 2, but starting from a larger power
-    # assert do_bees_meet(300, sequence_1, sequence_2) is True
-    # assert do_bees_meet(140, "1,2,4,8,16", "2,6,18,54,162") is True
+    sequence_1 = ",".join(str(x) for x in range(50000, 200001, 10000))  # Arithmetic sequence with a large difference
+    sequence_2 = ",".join(str(2 ** x) for x in range(30, 45))  # Geometric sequence with a ratio of 2, but starting from a larger power
+    assert do_bees_meet(300, sequence_1, sequence_2) is True
+    # assert do_bees_meet(140, "1,2,4,8,16", "2,6,18,54,162") is False
     # print(do_bees_meet(7, "2,6,12,20", "1,3,6,10"))
     # print(do_bees_meet(11, "1,2,3,4,5", "5,11,17,23,29"))
     # print(do_bees_meet(6, "1,2,4,7,11", "5,9,17,29,45"))
-    print(cells_count(15))
-    print(do_bees_meet(15, "1,3,7,15,31", "5,7,13,31,85"))
+    # print(do_bees_meet(15, "1,3,7,15,31", "5,7,13,31,85"))
     # print(do_bees_meet(10, "10,19,27,34,40", "50,47,44,41,38"))
