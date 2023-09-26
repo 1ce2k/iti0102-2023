6234b0da5e5da2072484b04f490d81cf57abceb9
Danil Zimarev <dazima@taltech.ee>
2023-09-26 16:52:08 +0300

    op lists.py fix

--- OP/op05_lists/lists.py
+++ OP/op05_lists/lists.py
@@ -38,7 +38,7 @@ def phone_brand_and_models(all_phones: str):
     return ret
 
 
-def add_phones(phone_list, all_phones) -> list:
+def add_phones(phone_list: list, all_phones: str) -> list:
     """
     Add phones from the list into the existing phone list.
 
@@ -54,8 +54,23 @@ def add_phones(phone_list, all_phones) -> list:
 
     [['IPhone', ['11', '12']], ['Google', ['Pixel']], ['Samsung', ['Galaxy S22']]]
     """
-
-    return []
+    ret = phone_list
+    list1 = []
+    phones = all_phones.split(',')
+    for x in ret:
+        for phone in phones:
+            brand = phone.split(' ')[0]
+            model = phone.replace(brand + ' ', '')
+            if brand in x[0]:
+                if model not in x[1]:
+                    x[1].append(model)
+            else:
+                list1.append(brand)
+                list1.append([model])
+                ret.append(list1)
+                list1 = []
+        break
+    return ret
 
 
 def number_of_phones(all_phones: str) -> list:
@@ -98,7 +113,7 @@ def phone_list_as_string(phone_list: list) -> str:
 
 
 if __name__ == "__main__":
-    print(phone_brand_and_models(
-        "Honor Magic5,IPhone 11,IPhone 12,Google Pixel,Samsung Galaxy S22,IPhone 13,IPhone 13,Google Pixel2"))
-    print(number_of_phones('Iphone 12,samsung 9,huawei honor,Iphone 13'))
-    print(phone_list_as_string([['IPhone', ['11', '12']], ['Google', ['Pixel']]]))
+    # print(phone_brand_and_models("Honor Magic5,IPhone 11,IPhone 12,Google Pixel,Samsung Galaxy S22,IPhone 13,IPhone 13,Google Pixel2"))
+    print(add_phones([['IPhone', ['11']], ['Google', ['Pixel']]], "IPhone 12,Samsung S22"))
+    # print(number_of_phones('Iphone 12,samsung 9,huawei honor,Iphone 13'))
+    # print(phone_list_as_string([['IPhone', ['11', '12']], ['Google', ['Pixel']]]))
