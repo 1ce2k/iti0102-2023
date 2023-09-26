db110caf497a5fa16187209f0a3292baf686b94c
Danil Zimarev <dazima@taltech.ee>
2023-09-26 13:52:40 +0300

    lists.py init

---
+++ EX/ex05_lists/lists.py
@@ -0,0 +1,42 @@
+"""Phone inventory."""
+
+
+def list_of_phones(all_phones: str) -> list:
+    """
+    Return list of phones.
+
+    The input string contains of phone brands and models, separated by comma.
+    Both the brand and the model do not contain spaces (both are one word).
+
+    "Google Pixel,Honor Magic5,Google Pixel" => ["Google Pixel', 'Honor Magic5', 'Google Pixel"]
+    """
+    return all_phones.split(',')
+
+
+def phone_brands(all_phones: str) -> list:
+    """
+    Return list of unique phone brands.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Google Pixel,Honor Magic5,Google Pixel" => ["Google", "Honor"]
+    """
+    ret = []
+    phones = all_phones.split(',')
+    brands = []
+    for phone in phones:
+        brand = phone.split(' ')
+        if brand not in brands:
+            brands.append(brand)
+    return brands
+
+
+def phone_models(all_phones: str) -> list:
+    """
+    Return list of unique phone models.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Honor Magic5,Google Pixel,Honor Magic4" => ['Magic5', 'Pixel', 'Magic4']
+    """
+    return []
