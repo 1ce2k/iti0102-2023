9e25cbc628ccca2619a7a26c5ba6ea77a25eb008
Danil Zimarev <dazima@taltech.ee>
2023-10-02 19:36:20 +0300

    conversation.py catalan fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -231,20 +231,33 @@ def find_fibonacci_numbers(biggest_number: int) -> list:
     return fibonacci_sequence
 
 
-def find_catalan_numbers(biggest_number: int) -> list:
+def find_catalan_numbers(biggest_number: int, memo={}) -> list:
     """
     Find all Catalan numbers in range(end inclusive).
 
     Can be solved using recursion.
+    :param memo:
     :param biggest_number: all catalan numbers in range of biggest_number(included)
     https://en.wikipedia.org/wiki/Catalan_number
     :return: list of catalan numbers
     """
-    catalan_nums = [1]
-    for i in range(biggest_number):
-        catalan = math.comb(2 * i, i) // (i + 1)
-        catalan_nums.append(catalan)
-    return catalan_nums
+    if biggest_number < 0:
+        return []
+    if biggest_number == 0:
+        return [1]
+
+    if biggest_number in memo:
+        return memo[biggest_number]
+
+    catalan_numbers = [0] * (biggest_number + 1)
+    catalan_numbers[0] = 1
+
+    for i in range(1, biggest_number + 1):
+        for j in range(i):
+            catalan_numbers[i] += catalan_numbers[j] * catalan_numbers[i - j - 1]
+
+    memo[biggest_number] = catalan_numbers
+    return catalan_numbers
 
 
 if __name__ == '__main__':
