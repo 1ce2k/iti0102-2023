9751e81ec67343e6ff0a9bc2a18006b383de09c5
Danil Zimarev <dazima@taltech.ee>
2023-10-05 14:24:36 +0300

    conversation.py fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -418,8 +418,8 @@ def find_catalan_numbers(biggest_number: int) -> list:
 
 
 regex_a = r'(-?\s*\d*|-)\s*x2(?!2)'
-regex_b = r'(?<!x[1])(-?\s*\d*|-)\s*x(?!2)'
-regex_c = r'(?<!x[1])(?<!x2)(-?\s*\d+)(?=\s|$)'
+regex_b = r'(?<!x[12])(-?\s*\d*|-)\s*x(?!2)'
+regex_c = r'(?<!x)(?<!x2)(-?\s*\d+)(?=\s|$)'
 
 
 if __name__ == "__main__":
