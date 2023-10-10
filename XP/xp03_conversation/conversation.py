48943385060ef7efdb9a59501eec4081581af659
Danil Zimarev <dazima@taltech.ee>
2023-10-10 16:34:27 +0300

    XP3 fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -142,24 +142,28 @@ class Student:
             elif len(solutions) == 1:
                 res = solutions * multiplicative
             self.deal_with_dec_value(str(int(res)))
+            self.possible_answers = '1'
         elif to_multiply and not is_bigger:
             if len(solutions) > 1:
                 res = min(solutions) * multiplicative
             elif len(solutions) == 1:
                 res = solutions * multiplicative
             self.deal_with_dec_value(str(int(res)))
+            self.possible_answers = '2'
         elif not to_multiply and is_bigger:
             if len(solutions) > 1:
                 res = max(solutions) / multiplicative
             elif len(solutions) == 1:
                 res = solutions / multiplicative
             self.deal_with_dec_value(str(int(res)))
+            self.possible_answers = '3'
         elif not to_multiply and not is_bigger:
             if len(solutions) > 1:
                 res = min(solutions) / multiplicative
             elif len(solutions) == 1:
                 res = solutions / multiplicative
             self.deal_with_dec_value(str(int(res)))
+            self.possible_answers = '4'
 
     def deal_with_fibonacci_sequence(self, is_in: bool):
         """
