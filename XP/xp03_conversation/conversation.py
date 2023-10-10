2a2fddaacd54e7e9280d3027c271dd2a31320309
Danil Zimarev <dazima@taltech.ee>
2023-10-10 17:14:52 +0300

    XP3 fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -357,22 +357,17 @@ def find_catalan_numbers(biggest_number: int) -> list:
     #     next = (4 * n - 2) * catalan_numbers[-1] // (n + 1)
     #     catalan_numbers.append(next)
     # return catalan_numbers
-    if (biggest_number == 0 or biggest_number == 1):
+
+    if biggest_number < 1:
         return []
-        # Table to store results of subproblems
-    catalan = [0 for i in range(biggest_number + 1)]
 
-    # Initialize first two values in table
+    catalan = [0] * (biggest_number + 1)
     catalan[0] = 1
-    catalan[1] = 1
 
-    # Fill entries in catalan[] using recursive formula
-    for i in range(2, biggest_number + 1):
-        catalan[i] = 0
+    for i in range(1, biggest_number + 1):
         for j in range(i):
             catalan[i] += catalan[j] * catalan[i - j - 1]
 
-    # Return last entry
     return catalan
 
 
