0069632774ef23cb1048ee3db72edda75e156d9a
Danil Zimarev <dazima@taltech.ee>
2023-10-10 16:45:06 +0300

    XP3 fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -160,7 +160,7 @@ class Student:
                 # self.possible_answers = '3'
             else:
                 if len(solutions) > 1:
-                    res = max(solutions) / multiplicative
+                    res = min(solutions) / multiplicative
                 elif len(solutions) == 1:
                     res = solutions / multiplicative
                 self.deal_with_dec_value(str(int(res)))
