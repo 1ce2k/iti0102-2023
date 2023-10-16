01e1e474150cfddc6b5ec928c4b2d3f6f25e6ffe
Danil Zimarev <dazima@taltech.ee>
2023-10-16 12:59:33 +0300

    stile fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -157,8 +157,8 @@ class Student:
                 # self.deal_with_dec_value(f'{max(solutions) / multiplicative:.0f}')
                 self.possible_answers = f'{max(solutions) / multiplicative:.0f}, {solutions}, {multiplicative}'
             elif not to_multiply and not is_bigger:
-                # self.deal_with_dec_value(f'{min(solutions) / multiplicative:.0f}')
-                self.possible_answers = f'{min(solutions) / multiplicative:.0f}, {solutions}, {multiplicative}'
+                self.deal_with_dec_value(f'{min(solutions) / multiplicative:.0f}')
+                # self.possible_answers = f'{min(solutions) / multiplicative:.0f}, {solutions}, {multiplicative}'
         if to_multiply and is_bigger:
             # self.deal_with_dec_value(f'{max(solutions) * multiplicative:.0f}')
             self.possible_answers = f'{max(solutions) * multiplicative:.0f}, {solutions}, {multiplicative}'
