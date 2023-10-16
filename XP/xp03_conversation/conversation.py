0a38e6096379ebd25a78cad2788eb8bcf18fb674
Danil Zimarev <dazima@taltech.ee>
2023-10-16 12:43:35 +0300

    stile fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -155,10 +155,10 @@ class Student:
             if to_multiply:
                 x = int(x * multiplicative)
                 self.deal_with_dec_value(f'{x * multiplicative:.0f}')
-                self.possible_answers = x
+                self.possible_answers = f'{x * multiplicative:.0f}'
             else:
                 self.deal_with_dec_value(f'{x / multiplicative:.0f}')
-                # self.possible_answers = x
+                self.possible_answers = x
 
     def deal_with_fibonacci_sequence(self, is_in: bool):
         """
