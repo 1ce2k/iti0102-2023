99e27f6c1b780145e4f29609a6826324dc9ee262
Danil Zimarev <dazima@taltech.ee>
2023-10-16 18:29:12 +0300

    fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -29,9 +29,7 @@ class Student:
         if the result is certain
         f"The number I needed to guess was {final_answer}."
         """
-
-        if re.findall(r"(occur to be a prime number|have to be a prime number)", sentence):
-            self.deal_with_primes(True) if re.findall(r"(occur to be a prime number|have to be a prime number)", sentence) else self.deal_with_composites(True)
+        self.deal_with_primes(True) if re.search(r"(occur to be a prime number|have to be a prime number)", sentence) else self.deal_with_primes(False)
 
         if len(self.possible_answers) == 1:
             return f"The num I needed to guess was {self.possible_answers}."
