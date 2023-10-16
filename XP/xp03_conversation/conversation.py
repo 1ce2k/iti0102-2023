c9ab09d819240b07aedf2ca24b1d3fb404d570cc
Danil Zimarev <dazima@taltech.ee>
2023-10-16 18:00:49 +0300

    fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -35,8 +35,8 @@ class Student:
 
         if len(self.possible_answers) == 1:
 
-            return f"The num I needed to guess was {self.possible_answers}"
-        return f"Possible answers are {list(self.possible_answers)}"
+            return f"The num I needed to guess was {self.possible_answers}."
+        return f"Possible answers are {list(self.possible_answers)}."
 
     def intersect_possible_answers(self, update: list):
         """
