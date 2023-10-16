3518136d6f5c4c3ac850fb3aced4d79d7378f3ab
Danil Zimarev <dazima@taltech.ee>
2023-10-16 12:57:30 +0300

    stile fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -155,16 +155,16 @@ class Student:
         if multiplicative != 0:
             if not to_multiply and is_bigger:
                 # self.deal_with_dec_value(f'{max(solutions) / multiplicative:.0f}')
-                self.possible_answers = f'{max(solutions) / multiplicative:.0f}, {solutions}'
+                self.possible_answers = f'{max(solutions) / multiplicative:.0f}, {solutions}, {multiplicative}'
             elif not to_multiply and not is_bigger:
-                self.deal_with_dec_value(f'{min(solutions) / multiplicative:.0f}')
-                # self.possible_answers = f'{min(solutions) / multiplicative:.0f}, {solutions}'
+                # self.deal_with_dec_value(f'{min(solutions) / multiplicative:.0f}')
+                self.possible_answers = f'{min(solutions) / multiplicative:.0f}, {solutions}, {multiplicative}'
         if to_multiply and is_bigger:
             # self.deal_with_dec_value(f'{max(solutions) * multiplicative:.0f}')
-            self.possible_answers = f'{max(solutions) * multiplicative:.0f}, {solutions}'
+            self.possible_answers = f'{max(solutions) * multiplicative:.0f}, {solutions}, {multiplicative}'
         elif to_multiply and not is_bigger:
-            self.deal_with_dec_value(f'{min(solutions) * multiplicative:.0f}')
-            # self.possible_answers = f'{min(solutions) * multiplicative:.0f}, {solutions}'
+            # self.deal_with_dec_value(f'{min(solutions) * multiplicative:.0f}')
+            self.possible_answers = f'{min(solutions) * multiplicative:.0f}, {solutions}, {multiplicative}'
 
     def deal_with_fibonacci_sequence(self, is_in: bool):
         """
