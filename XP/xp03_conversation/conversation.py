e36f57afb1ef9c17718dc20909a1f8697e80c654
Danil Zimarev <dazima@taltech.ee>
2023-10-16 18:34:15 +0300

    fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -29,11 +29,15 @@ class Student:
         if the result is certain
         f"The number I needed to guess was {final_answer}."
         """
-        self.deal_with_primes(True) if re.search(r"(occur to be a prime number|have to be a prime number)", sentence) else self.deal_with_primes(True)
+        if re.search(r"(occur to be a prime number|have to be a prime number)", sentence):
+            self.deal_with_primes(True)
+        if re.search(r"(occur not to be a prime number|have not to be a prime number)", sentence):
+            self.deal_with_primes(False)
+
 
         if len(self.possible_answers) == 1:
             return f"The num I needed to guess was {self.possible_answers}."
-        sorted_list = [sorted(self.possible_answers)]
+        sorted_list = sorted(self.possible_answers)
         return f"Possible answers are {sorted_list}."
 
     def intersect_possible_answers(self, update: list):
