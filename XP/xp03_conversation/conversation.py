4100b592d9bb15240c83d8a2741270a665b1b093
Danil Zimarev <dazima@taltech.ee>
2023-10-22 22:40:04 +0300

    fix

--- XP/xp03_conversation/conversation.py
+++ XP/xp03_conversation/conversation.py
@@ -50,7 +50,7 @@ class Student:
         """Deal with binary form, func to make decision branch less complex."""
         if re.search(r'one', sentence):
             self.deal_with_number_of_ones(int(re.search(r'\d+', sentence).group()))
-        elif re.search(r'zero', sentence):
+        if re.search(r'zero', sentence):
             self.deal_with_number_of_zeroes(int(re.search(r'\d+', sentence).group()))
 
     def equation(self, sentence):
