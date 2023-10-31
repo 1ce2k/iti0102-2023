d6d9bc5d40406a0d8522429f3c1aeca5c519849f
Danil Zimarev <dazima@taltech.ee>
2023-10-31 19:52:07 +0200

    OP09

---
+++ OP/op09_files/file_handling.py
@@ -0,0 +1,97 @@
+import csv
+from datetime import datetime
+
+
+def read_csv_file_into_list_of_dicts_using_datatypes(filename: str) -> list[dict]:
+    """
+    Read data from a CSV file and cast values into different data types based on their content.
+
+    Fields containing only numbers are cast into integers.
+    Fields containing dates (in the format dd.mm.yyyy) are cast into date.
+    Otherwise, the data type remains string (default by csv reader).
+    The order of elements in the list matches the lines in the file.
+    None values don't affect the data type (the column will have the type based on the existing values).
+
+    Hint: For date parsing, you can use the strptime method. See examples here:
+    https://docs.python.org/3/library/datetime.html#examples-of-usage-date
+
+    If a field contains only numbers, it's cast to int:
+    name,age
+    john,11
+    mary,14
+
+    Will become ('age' is int):
+    [
+      {'name': 'john', 'age': 11},
+      {'name': 'mary', 'age': 14}
+    ]
+
+    If a field contains text or mixed content, it remains as a string:
+    name,age
+    john,11
+    mary,14
+    ago,unknown
+
+    Will become ('age' cannot be cast to int because of "ago"):
+    [
+      {'name': 'john', 'age': '11'},
+      {'name': 'mary', 'age': '14'},
+      {'name': 'ago', 'age': 'unknown'}
+    ]
+
+    If a field contains only dates, it's cast to date:
+    name,date
+    john,01.01.2022
+    mary,07.09.2023
+
+    Will become:
+    [
+      {'name': 'john', 'date': datetime.date(2022, 1, 1)},
+      {'name': 'mary', 'date': datetime.date(2023, 9, 7)},
+    ]
+
+    Example:
+    name,date
+    john,01.01.2022
+    mary,late 2023
+
+    Will become:
+    [
+      {'name': 'john', 'date': "01.01.2022"},
+      {'name': 'mary', 'date': "late 2023"},
+    ]
+
+    A missing value "-" is interpreted as None (data type is not affected):
+    name,date
+    john,-
+    mary,07.09.2023
+
+    Will become:
+    [
+      {'name': 'john', 'date': None},
+      {'name': 'mary', 'date': datetime.date(2023, 9, 7)},
+    ]
+
+    :param filename: The name of the CSV file to read.
+    :return: A list of dictionaries containing processed field values.
+    """
+    result = []
+
+    with open(filename, 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            processed_row = {}
+            for key, value in row.items():
+                if value == "-":
+                    processed_row[key] = None
+                elif value.replace('.', '', 1).isdigit():
+                    processed_row[key] = int(value)
+                elif len(value) == 10 and value[2] == '.' and value[5] == '.' and value[0:2].isdigit() and value[3:5].isdigit() and value[6:].isdigit():
+                    processed_row[key] = datetime.strptime(value, '%d.%m.%Y').date()
+                else:
+                    processed_row[key] = value
+            result.append(processed_row)
+    return result
+
+
+print(read_csv_file_into_list_of_dicts_using_datatypes('../../EX/ex09_files/input.csv'))
