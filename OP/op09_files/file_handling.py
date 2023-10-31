8751a369e49e28a6ea40ef3547838ddd71733b37
Danil Zimarev <dazima@taltech.ee>
2023-10-31 21:35:36 +0200

    OP09

--- OP/op09_files/file_handling.py
+++ OP/op09_files/file_handling.py
@@ -1,4 +1,5 @@
 import csv
+import re
 from datetime import datetime
 
 
@@ -75,23 +76,88 @@ def read_csv_file_into_list_of_dicts_using_datatypes(filename: str) -> list[dict
     :param filename: The name of the CSV file to read.
     :return: A list of dictionaries containing processed field values.
     """
-    result = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        keys = next(reader)
+        res = [[] for _ in range(len(keys))]
+        converted_data = []
+        for line in reader:
+            line_parts = line
+            for i in range(len(keys)):
+                res[i].append(line_parts[i])
+        for item in res:
+            is_digits = all_elements_int_or_dash(item)
+            is_dates = all_elements_date_or_dash(item)
+            temp = []
+            if is_digits:
+                temp = convert_to_int(item)
+            elif is_dates:
+                temp = convert_to_dates(item)
+            else:
+                temp = convert_strings(item)
+            converted_data.append(temp)
+    ret = [{key: value[i] for key, value in zip(keys, converted_data)} for i in range(len(converted_data[0]))]
+    return ret
 
-    with open(filename, 'r', newline='') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            processed_row = {}
-            for key, value in row.items():
-                if value == "-":
-                    processed_row[key] = None
-                elif value.replace('.', '', 1).isdigit():
-                    processed_row[key] = int(value)
-                elif len(value) == 10 and value[2] == '.' and value[5] == '.' and value[0:2].isdigit() and value[3:5].isdigit() and value[6:].isdigit():
-                    processed_row[key] = datetime.strptime(value, '%d.%m.%Y').date()
-                else:
-                    processed_row[key] = value
-            result.append(processed_row)
-    return result
+
+def all_elements_int_or_dash(input_list: list):
+    """Check if all elements are same type."""
+    for item in input_list:
+        if not (item.isdigit() or item == '-'):
+            return False
+    return True
 
 
-# print(read_csv_file_into_list_of_dicts_using_datatypes('../../EX/ex09_files/input.csv'))
+def all_elements_date_or_dash(data: list):
+    """Check if all elements are same type."""
+    date_pattern = r'\d{2}\.\d{2}.\d{4}'
+    for item in data:
+        if item != '-' and not re.match(date_pattern, item) or (item != '-' and not is_valid_date(item)):
+            return False
+    return True
+
+
+def is_valid_date(date_str):
+    """Check if date exists."""
+    day, month, year = map(int, date_str.split('.'))
+    if (1 <= month <= 12) and (1 <= day <= 31):
+        if month in [4, 6, 9, 11] and day > 30:
+            return False
+        if month == 2:
+            if (day > 29) or (day == 29 and not (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))):
+                return False
+        return True
+    return False
+
+
+def convert_to_int(data: list):
+    """Convert from str to int."""
+    res = []
+    for element in data:
+        if element.isdigit():
+            res.append(int(element))
+        elif element == '-':
+            res.append(None)
+    return res
+
+
+def convert_to_dates(data: list):
+    """Convert from str t0 datetime.date."""
+    res = []
+    for element in data:
+        if element != '-':
+            res.append(datetime.strptime(element, '%d.%m.%Y').date())
+        else:
+            res.append(None)
+    return res
+
+
+def convert_strings(data: list):
+    """Convert from - to None in str."""
+    res = []
+    for item in data:
+        res.append(None) if item == '-' else res.append(item)
+    return res
+
+
+print(read_csv_file_into_list_of_dicts_using_datatypes('../../EX/ex09_files/input.csv'))
