4a63c8e2df511c69aeebb19e886174f74f22be12
Danil Zimarev <dazima@taltech.ee>
2023-10-31 23:23:04 +0200

    WAT

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,34 +1,38 @@
-def function_a(x):
+def function_a(x: int) -> int:
     pass
 
 
-def function_b(x):
+def function_b(x: int) -> int:
     pass
 
 
-def function_d(x):
+def function_c(x: int) -> int:
     pass
 
 
-def function_e(x):
+def function_d(x: int) -> int:
     pass
 
 
-def function_f(x):
+def function_e(x: int) -> int:
     pass
 
 
-def function_g(x):
+def function_f(x: int) -> int:
+    return x
+
+
+def function_g(x: int) -> int:
     pass
 
 
-def function_h(x):
+def function_h(x: int) -> int:
     pass
 
 
-def function_i(x):
+def function_i(x: int) -> int:
     pass
 
 
-def function_j(x):
+def function_j(x: int) -> int:
     pass
