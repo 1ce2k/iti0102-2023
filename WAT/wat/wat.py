9060350bf84adb184dbf6f37bd9ad4acc2c83543
Danil Zimarev <dazima@taltech.ee>
2023-11-01 11:05:59 +0200

    OP09

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,38 +1,48 @@
 def function_a(x: int) -> int:
+    """Return 1."""
     return 1
 
 
 def function_b(x: int) -> int:
+    """Return x + 11."""
     return x + 11
 
 
 def function_c(x: int) -> int:
+    """Return x in power 8."""
     return x ** 8
 
 
 def function_d(x: int) -> int:
+    """Return x in power 2 multiplied by 36."""
     return x ** 2 * 36
 
 
 def function_e(x: int) -> int:
+    """Return x multiplied by 32."""
     return x * 32
 
 
 def function_f(x: int) -> int:
+    """Some func."""
     return x
 
 
 def function_g(x: int) -> int:
+    """Return -x."""
     return -x
 
 
 def function_h(x: int) -> int:
+    """Some func."""
     return x
 
 
 def function_i(x: int) -> int:
+    """Return 0."""
     return 0
 
 
 def function_j(x: int) -> int:
+    """Some func."""
     return x * 2
