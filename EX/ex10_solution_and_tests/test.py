f17adde318edbfae3e9e15e82ce07f711de333d9
Danil Zimarev <dazima@taltech.ee>
2023-11-07 11:22:11 +0200

    init

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -6,14 +6,26 @@ from solution import lottery
 from solution import fruit_order
 
 
-def test_students_study():
-    assert students_study(12, True) is True
-    assert students_study(5, True) is True
-    assert students_study(5, False) is False
-    assert students_study(17, True) is True
+def test_students_study_evening():
+    assert students_study(20, True) is True
+    assert students_study(20, False) is True
+    assert students_study(18, False) is True
     assert students_study(18, True) is True
-    assert students_study(1, True) is False
-    assert students_study(23, True) is True
     assert students_study(24, True) is True
     assert students_study(24, False) is True
 
+
+def test_students_study_night():
+    assert students_study(1, True) is False
+    assert students_study(1, False) is False
+    assert students_study(4, True) is False
+    assert students_study(4, False) is False
+
+
+def test_students_study_day():
+    assert students_study(5, True) is True
+    assert students_study(5, False) is False
+    assert students_study(7, True) is True
+    assert students_study(8, False) is False
+    assert students_study(17, True) is True
+    assert students_study(17, False) is False
