9db3a932a067df6b56d57743111198ed85707701
Danil Zimarev <dazima@taltech.ee>
2023-11-07 12:14:33 +0200

    init

--- OP/op10_password_test/test.py
+++ OP/op10_password_test/test.py
@@ -3,16 +3,9 @@ import password
 
 
 def test__is_correct_length__too_short():
-    """Test whether password of length 7 is not correct."""
+    """Test whether password is correct length or not."""
     assert password.is_correct_length("passwor") is False
     assert password.is_correct_length('') is False
-
-
-def test__is_correct_length__too_long():
-    """Test whether password of length > 64 is incorrect."""
     assert password.is_correct_length("a" * 65) is False
-
-
-def test_is_correct_length_true():
     assert password.is_correct_length('password') is True
     assert password.is_correct_length('a' * 64) is True
