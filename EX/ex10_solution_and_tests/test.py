980408731aff346b13dad01f83f97712a8969633
Danil Zimarev <dazima@taltech.ee>
2023-11-07 11:23:11 +0200

    init

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -7,6 +7,7 @@ from solution import fruit_order
 
 
 def test_students_study_evening():
+    """Tests students_study evening time."""
     assert students_study(20, True) is True
     assert students_study(20, False) is True
     assert students_study(18, False) is True
@@ -16,6 +17,7 @@ def test_students_study_evening():
 
 
 def test_students_study_night():
+    """Tests students_study night time."""
     assert students_study(1, True) is False
     assert students_study(1, False) is False
     assert students_study(4, True) is False
@@ -23,6 +25,7 @@ def test_students_study_night():
 
 
 def test_students_study_day():
+    """Tests students_study day time."""
     assert students_study(5, True) is True
     assert students_study(5, False) is False
     assert students_study(7, True) is True
