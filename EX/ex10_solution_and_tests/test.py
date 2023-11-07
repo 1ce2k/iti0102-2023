05ffd4819424db5d1591e08fa64b827f6b307c41
Danil Zimarev <dazima@taltech.ee>
2023-11-07 11:12:16 +0200

    init

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -8,3 +8,9 @@ from solution import fruit_order
 
 def test_students_study():
     assert students_study(12, True) is True
+    assert students_study(5, True) is True
+    assert students_study(17, True) is True
+    assert students_study(18, True) is True
+    assert students_study(18, False) is True
+    assert students_study(1, True) is False
+    assert students_study(1, False) is True
