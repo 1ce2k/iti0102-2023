448d21d9f7ddbb7ffee490c0eac8945b415d61e4
Danil Zimarev <dazima@taltech.ee>
2023-11-07 12:11:19 +0200

    init

--- OP/op10_password_test/test.py
+++ OP/op10_password_test/test.py
@@ -5,8 +5,6 @@ from EX.ex04_validation import password
 def test__is_correct_length__too_short():
     """Test whether password of length 7 is not correct."""
     assert password.is_correct_length("passwor") is False
-    assert password.is_correct_length('password') is True
-    assert password.is_correct_length('password1') is True
     # pass_1 = 'a' * 64
     # pass_2 = 'a' * 65
     # assert password.is_correct_length(pass_1) is True
