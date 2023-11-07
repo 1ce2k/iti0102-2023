3ddf5390ff7622ead4748e369da3a530e7e7986d
Danil Zimarev <dazima@taltech.ee>
2023-11-07 11:13:22 +0200

    init

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -11,6 +11,4 @@ def test_students_study():
     assert students_study(5, True) is True
     assert students_study(17, True) is True
     assert students_study(18, True) is True
-    assert students_study(18, False) is True
-    assert students_study(1, True) is False
-    assert students_study(1, False) is True
+
