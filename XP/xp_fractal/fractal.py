663e5a1383c88691a9bc41c6ad7049f5364eb486
Danil Zimarev <dazima@taltech.ee>
2023-11-07 18:03:04 +0200

    fracctal init

---
+++ XP/xp_fractal/fractal.py
@@ -0,0 +1,119 @@
+"""Create beautiful fractal images."""
+from PIL import Image
+
+
+class Fractal:
+    """Fractal class."""
+
+    def __init__(self, size, scale, computation):
+        """
+        Initialize fractal.
+
+        Arguments:
+        size -- the size of the image as a tuple (x, y)
+        scale -- the scale of x and y as a list of 2-tuple
+                 [(minimum_x, minimum_y), (maximum_x, maximum_y)]
+                 these are mathematical coordinates
+        computation -- the function used for computing pixel values as a function
+        """
+        self.size = size
+        self.scale = scale
+        self.computation = computation
+        self.image = Image.new("RGB", size)
+
+    def compute(self):
+        """Create the fractal by computing every pixel value."""
+        for x in range(self.size[0]):
+            for y in range(self.size[1]):
+                x_scaled = self.scale[0][0] + (x / self.size[0]) * (self.scale[1][0] - self.scale[0][0])
+                y_scaled = self.scale[0][1] + (y / self.size[1]) * (self.scale[1][1] - self.scale[0][1])
+                pixel = x_scaled, y_scaled
+                iterations = self.pixel_value(pixel)
+                color = (iterations, iterations, iterations)
+
+    def pixel_value(self, pixel):
+        """
+        Return the number of iterations it took for the pixel to go out of bounds.
+
+        Arguments:
+        pixel -- the pixel coordinate (x, y)
+
+        Returns:
+        the number of iterations of computation it took to go out of bounds as integer.
+        """
+        return self.computation(pixel, pixel)
+
+    def save_image(self, filename):
+        """
+        Save the image to hard drive.
+
+        Arguments:
+        filename -- the file name to save the file to as a string.
+        """
+        self.image.save(filename)
+
+
+def mandelbrot_computation(x: float, y: float, x_original: float, y_original: float) -> tuple:
+    """
+    Return single iteration result of computation as tuple[x, y].
+
+    :param x - mathematical x coordinate (transformed)
+    :param y - mathematical y coordinate (transformed)
+    :param x_original - mathematical x coordinate of pixel
+    :param y_original - mathematical y coordinate of pixel
+
+    :return tuple[x, y] after single iteration
+    """
+    max_iterations = 1000
+    z = 0
+    c = complex(x, y)
+    for i in range(max_iterations):
+        z = z * z + c
+        if abs(z) > 2:
+            return i
+    return max_iterations
+
+
+def julia_computation(x: float, y: float, x_original: float, y_original: float) -> tuple:
+    """
+    Return single iteration result of computation as tuple[x, y].
+
+    For different c and n make new function rather than change these constants.
+    Otherwise tester will not give you any points :p
+
+    :param x - mathematical x coordinate (transformed)
+    :param y - mathematical y coordinate (transformed)
+    :param x_original - mathematical x coordinate of pixel
+    :param y_original - mathematical y coordinate of pixel
+
+    :return tuple[x, y] after single iteration
+    """
+    c = -0.7869 + 0.1889j  # DO NOT CHANGE
+    n = 3  # DO NOT CHANGE
+
+    pass
+
+
+def ship_computation(x: float, y: float, x_original: float, y_original: float) -> tuple:
+    """
+    Return single iteration result of computation as tuple[x, y].
+
+    You should invert y axis for correct results and picture
+
+    :param x - mathematical x coordinate (transformed)
+    :param y - mathematical y coordinate (transformed)
+    :param x_original - mathematical x coordinate of pixel
+    :param y_original - mathematical y coordinate of pixel
+
+    :return tuple[x, y] after single iteration
+    """
+    pass
+
+
+if __name__ == "__main__":
+    mandelbrot = Fractal((1000, 1000), [(-2, -2), (2, 2)], mandelbrot_computation)
+    mandelbrot.compute()
+    mandelbrot.save_image("mandelbrot.png")
+    mandelbrot2 = Fractal((1000, 1000), [(-0.74877, 0.065053), (-0.74872, 0.065103)], mandelbrot_computation)
+    mandelbrot2.compute()
+    mandelbrot2.save_image("mandelbrot2.png")
