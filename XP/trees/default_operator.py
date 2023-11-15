a6fd54ee94cd550534bd5f7e8f694ea2cf1b32a9
Danil Zimarev <dazima@taltech.ee>
2023-11-15 17:15:37 +0200

    trees default_operator.py init

--- XP/trees/default_operator.py
+++ XP/trees/default_operator.py
@@ -5,14 +5,18 @@ class DefaultOperator:
     """Default operator is a wrapper to a mathematical function with a string form."""
 
     def __init__(self, function_to_apply, string_repr_of_func):
+        """Init function."""
         self.function = function_to_apply
         self.representation = string_repr_of_func
 
     def __call__(self, *args):
+        """Call funtion."""
         return self.function(*args)
 
     def __repr__(self):
+        """Func to represent."""
         return self.representation
+
 
 if __name__ == '__main__':
     operator = DefaultOperator(lambda x, y: x + y, "+")
