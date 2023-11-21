f21b44b8151268aef8e7e93d5b39f6fd66747604
Danil Zimarev <dazima@taltech.ee>
2023-11-21 12:16:55 +0200

    test push

--- XP/trees/operators/operator.py
+++ XP/trees/operators/operator.py
@@ -17,11 +17,11 @@ class Operator(TreeNode):
         return self.default_operator(*[x.apply() for x in self.__value])
 
     def class_str(self):
-        """:return class string representation of the object."""
-        return "Add(Leaf(5), Leaf(6))"
+        """Return class string representation of the object."""
+        return f"{self.__class__.__name__}({', '.join([x.class_str() for x in self.__value])})"
 
     def __str__(self):
-        """:return the mathematical string representation of the tree with least amount of parenthesis."""
+        """Return the mathematical string representation of the tree with the least amount of parenthesis."""
         # return "5 + 6"
         return f" {self.default_operator} ".join([self.__encase(x) for x in self.__value])
 
@@ -39,20 +39,20 @@ class Operator(TreeNode):
 
     @property
     def associativity(self):
-        """:abstract method witch should be overridden to return a boolean when the node is not associative."""
+        """Abstract method witch should be overridden to return a boolean when the node is not associative."""
         return False
 
     @property
     @abstractmethod
     def default_operator(self):
-        """:abstract method which should be overridden to return the default_operator object."""
+        """Abstract method which should be overridden to return the default_operator object."""
         pass
 
     @property
     @abstractmethod
     def priority(self):
         """
-        :abstract method witch should be overridden to return priority of the node.
+        Abstract method witch should be overridden to return priority of the node.
 
         Boolean whether the operation is associative or not.
         For example addition is associative but subtraction is not.
@@ -68,7 +68,7 @@ class Operator(TreeNode):
         All custom implemented actions on different data structures.
 
         For example set - int does not exist, but we can implement it.
-        :return a dictionary of functions where key is accepted parameters and value is a function which takes the
+        Return a dictionary of functions where key is accepted parameters and value is a function which takes the
         aforementioned parameters as inputs and computes a value with them.
         """
         pass
