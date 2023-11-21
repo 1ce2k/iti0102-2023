7adbab0550bc00806729d4e6ab5c5b24218edd5b
Danil Zimarev <dazima@taltech.ee>
2023-11-21 12:31:45 +0200

    investor.py init

---
+++ XP/xp_investor/investor.py
@@ -0,0 +1,47 @@
+"""Investor helper."""
+import csv
+
+
+def get_currency_rates_from_file(filename: str) -> tuple:
+    """
+    Read and return the currency and exchange rate history from file.
+
+    See web page:
+    https://www.eestipank.ee/valuutakursside-ajalugu
+
+    Note that the return value is tuple, that consists of two things:
+    1) currency name given in the file.
+    2) exchange rate history for the given currency.
+        Note that history is returned using dictionary where keys represent dates
+        and values represent exchange rates for the dates.
+
+    :param filename: file name to read CSV data from
+    :return: Tuple that consists of currency name and dict with exchange rate history
+    """
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+    return reader
+
+
+def exchange_money(exchange_rates: dict) -> list:
+    """
+    Find best dates to exchange money for maximum profit.
+
+    You are given a dictionary where keys represent dates and values represent exchange
+    rates for the dates. The amount you initially have is 1000 and you always use the
+    maximum amount during the exchange.
+    Be aware that there is 1% of service fee for every exchange. You only need to return
+    the dates where you take action. That means the first action is always to buy the
+    second currency and the second action is to sell it back. Repeat the sequence as
+    many times as you need for maximum profit. You should always end up having the
+    initial currency. That means there should always be an even number of actions. You can
+    also decide that the best decision is to not make any transactions at all, if
+    for example the rate is always dropping. In that case just return an empty list.
+
+    :param exchange_rates: dictionary of dates and exchange rates
+    :return: list of dates
+    """
+    return [
+        "2023-11-01",
+        "2023-11-07"
+    ]
