981165d2750dee0fc840834f5cff23be71a9123e
Danil Zimarev <dazima@taltech.ee>
2023-11-23 14:26:01 +0200

    EX12 test fix

--- EX/ex12_music/music.py
+++ EX/ex12_music/music.py
@@ -69,7 +69,7 @@ class NoteCollection:
 
         :param note: Input object to add to the collection
         """
-        if type(note) is Note:
+        if type(note) is Note and note not in self.notes:
             self.notes.append(note)
         else:
             raise TypeError
