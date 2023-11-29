ae55037b005958e94e8cf36ab109752e1899148b
Danil Zimarev <dazima@taltech.ee>
2023-11-29 18:57:32 +0200

    exam0 init

--- XP/trees/operators/and_.py
+++ XP/trees/operators/and_.py
@@ -15,7 +15,7 @@ class And(Operator):
     @property
     def priority(self):
         """:priority of the operation."""
-        return 10
+        return 1
 
     @property
     def default_operator(self):
