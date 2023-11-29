fac2284d328284572f0939a5f50869e2e64a63ec
Danil Zimarev <dazima@taltech.ee>
2023-11-29 14:41:58 +0200

    investor.py get currency test

--- XP/trees/operators/and_.py
+++ XP/trees/operators/and_.py
@@ -15,7 +15,7 @@ class And(Operator):
     @property
     def priority(self):
         """:priority of the operation."""
-        return -1
+        return -2
 
     @property
     def default_operator(self):
