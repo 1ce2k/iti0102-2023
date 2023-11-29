3e0037c7d746369cece4fc75676df5b157c1cd53
Danil Zimarev <dazima@taltech.ee>
2023-11-29 14:34:17 +0200

    investor.py get currency test

--- XP/trees/operators/pow.py
+++ XP/trees/operators/pow.py
@@ -15,7 +15,7 @@ class Pow(Operator):
     @property
     def priority(self):
         """:priority of the operation."""
-        return -1
+        return 2
 
     @property
     def default_operator(self):
