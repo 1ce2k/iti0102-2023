0902441575df2d778b69b37169801f11f451af88
Danil Zimarev <dazima@taltech.ee>
2023-11-29 18:58:24 +0200

    exam0 init

--- XP/trees/operators/or_.py
+++ XP/trees/operators/or_.py
@@ -15,7 +15,7 @@ class Or(Operator):
     @property
     def priority(self):
         """:priority of the operation."""
-        return 10
+        return 100
 
     @property
     def default_operator(self):
