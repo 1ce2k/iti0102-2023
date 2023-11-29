533b507f97b8ad3336d2dd1204fb88ebda781f37
Danil Zimarev <dazima@taltech.ee>
2023-11-29 18:44:09 +0200

    exam0 init

--- XP/trees/operators/mul.py
+++ XP/trees/operators/mul.py
@@ -28,5 +28,5 @@ class Mul(Operator):
         return {
             (set, set): lambda set1, set2: {frozenset((x, y)) for x in set1 for y in set2},  # cartesian product
             (set, int): lambda set1, integer: {frozenset((x, integer)) for x in set1},  # {1, 3} * 2 == {{1, 2}, {3, 2}}
-            (int, set): {}  # 2 * {1, 3} == {{2, 1}, {2, 3}}
+            (int, set): lambda integer, set1: {frozenset((integer, x)) for x in set1}  # 2 * {1, 3} == {{2, 1}, {2, 3}}
         }
