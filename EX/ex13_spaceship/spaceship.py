9137af2ff1f5d994ea7ca1fda97fbbcfebc4e8ad
Danil Zimarev <dazima@taltech.ee>
2023-12-01 14:01:41 +0200

    ex13 spaceship fix

--- EX/ex13_spaceship/spaceship.py
+++ EX/ex13_spaceship/spaceship.py
@@ -5,7 +5,7 @@ class Crewmate:
     def __init__(self, color: str, role: str, tasks: int = 10):
         """Init crewmate."""
         self.color = color.capitalize()
-        self.role = role.title()
+        self.role = role.title() if role.title() != 'Impostor' else 'Crewmate'
         self.tasks_left = tasks
         self.alive = True
         self.protected = False
