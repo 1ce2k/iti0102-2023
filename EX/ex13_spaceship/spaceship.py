7135af2f6b30509787c6082b2674a3d373ce5f41
Danil Zimarev <dazima@taltech.ee>
2023-12-01 14:09:17 +0200

    ex13 spaceship fix

--- EX/ex13_spaceship/spaceship.py
+++ EX/ex13_spaceship/spaceship.py
@@ -104,6 +104,9 @@ class Spaceship:
     def get_impostor_with_most_kills(self):
         return sorted(self.impostors, key=lambda x: -x.kills)[0]
 
+    def get_crewmate_with_most_tasks_done(self):
+        return sorted(self.crewmate, key=lambda x: x.tasks_left)[0]
+
 
 if __name__ == "__main__":
     print("Spaceship.")
