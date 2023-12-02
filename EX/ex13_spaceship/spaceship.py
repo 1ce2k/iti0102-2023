fad607532dea340a54e9d187e1d4c8b174247085
Danil Zimarev <dazima@taltech.ee>
2023-12-02 21:40:52 +0200

    EX13 kill crewmate fix

--- EX/ex13_spaceship/spaceship.py
+++ EX/ex13_spaceship/spaceship.py
@@ -36,7 +36,7 @@ class Spaceship:
     def kill_impostor(self, killer, target_name):
         """Kill impostor."""
         target = next((impostor for impostor in self.impostor_list if impostor.name == target_name.capitalize()), None)
-        if target and killer in self.crewmate_list and killer.role == 'Sheriff' and target.role == 'Impostor' and target not in self.dead_players:
+        if target and killer in self.crewmate_list and killer.role == 'Sheriff' and target.role == 'Impostor':
             if not target.protected:
                 self.dead_players.append(target)
             else:
