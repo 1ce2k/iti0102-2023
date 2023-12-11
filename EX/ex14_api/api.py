8db9e21df041c444d83d39a5a2d97f28fee77dd4
Danil Zimarev <dazima@taltech.ee>
2023-12-11 11:58:23 +0200

    EX 14 init

--- EX/ex14_api/api.py
+++ EX/ex14_api/api.py
@@ -27,7 +27,7 @@ def get_request_error_handling(url: str) -> int | requests.RequestException:
     """
     try:
         return requests.get(url).status_code
-    except requests.RequestException as e:
+    except requests.exceptions.RequestException as e:
         return e
 
 
