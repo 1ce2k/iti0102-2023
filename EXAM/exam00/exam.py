ae7a7ed43bcf1a99826484467d2f63a8871083df
Danil Zimarev <dazima@taltech.ee>
2024-01-04 22:34:03 +0200

    EXAM 0 test

--- EXAM/exam00/exam.py
+++ EXAM/exam00/exam.py
@@ -68,13 +68,6 @@ def get_names_from_results(results_string: str, min_result: int) -> list:
     return name_list
 
 
-# print(get_names_from_results("ago 123,peeter 11", 0))
-# print(get_names_from_results("ago 123,peeter 11,33", 10))
-# print(get_names_from_results("ago 123,peeter 11", 100))
-# print(get_names_from_results("ago 123,peeter 11,kitty11!! 33", 11))
-# print(get_names_from_results("ago 123,peeter 11,kusti riin 14", 12))
-
-
 def tic_tac_toe(game: list) -> int:
     """
     Find game winner.
